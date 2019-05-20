dafab67c0e8e3f35e5741f00189cd2e8d8667dee
MdSlr704 <muhammadsalar704@gmail.com>
2019-05-20 16:10:19 +0430

    add edit and delete post

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.homepage),
     path('newpost/',views.new_post),
+    path('<int:id>/edit/',views.edit_post),
+    path('<int:id>/delete/',views.delete_post),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect, get_object_or_404
 from .models import Blog_post
 from .forms import New_post
 
@@ -16,3 +16,24 @@ def new_post (request):
     else:
         form = New_post()
     return render(request,'newpost.html',{'form':form})
+def edit_post (request,id):
+    obj = get_object_or_404(Blog_post,id = id)
+    if not request.user.is_authenticated:
+        return render(request, 'registration/loginfirst.html',{})
+    if request.method == 'POST':
+        form = New_post(request.POST, instance = obj)
+        if form.is_valid:
+            form.save()
+            return redirect('http://127.0.0.1:8000/blog/')
+    else:
+        form = New_post(instance = obj)
+    return render(request, 'newpost.html',{'form':form})
+def delete_post (request,id):
+    if not request.user.is_authenticated:
+        return render(request, 'registration/loginfirst.html',{})
+    if request.method == 'POST':
+        obj = get_object_or_404(Blog_post, id = id )
+        obj.delete()
+        return redirect('http://127.0.0.1:8000/blog/')
+    else:
+        return render(request,'deletepost.html',{})
